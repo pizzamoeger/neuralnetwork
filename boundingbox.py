2fc7f9686e5710732d72b7f04cbb08b83ae1d453
pizzamoeger <osshannahsophie@gmail.com>
2023-03-24 08:52:49 +0100

    added possibility to let the network predict a drawn number

---
+++ boundingbox.py
@@ -0,0 +1,71 @@
+from PIL import Image
+
+def boundingbox():
+    # Open the image file
+    image = Image.open("number.jpg")
+
+    # Get the size of the image
+    width, height = image.size
+
+    # Access the pixel values
+    pixels = list(image.getdata())
+
+    for i in range(0, len(pixels)):
+        if pixels[i] == (0, 0, 0):
+            pixels[i] = 0
+        else:
+            pixels[i] = 1
+
+    # find the bounding box
+    ymin = height
+    xmin = width
+    ymax = 0
+    xmax = 0
+
+    for i in range(0, len(pixels)):
+        if pixels[i] == 0:
+            continue
+        x = i % width
+        y = i // width
+        xmin = min(xmin, x)
+        xmax = max(xmax, x)
+        ymin = min(ymin, y)
+        ymax = max(ymax, y)
+
+    if (xmax - xmin) > (ymax - ymin):
+        newW = 20
+        newH = int((ymax-ymin)/(xmax-xmin)*20)
+    else:
+        newH = 20
+        newW = int((xmax-xmin)/(ymax-ymin)*20)
+
+    # crop the image
+    image = image.crop((xmin, ymin, xmax, ymax))
+
+    # resize the image so that the longest side is 20 pixels
+    image.thumbnail((newW, newH))
+
+    pixels = list(image.getdata())
+    for i in range(0, len(pixels)):
+        pixels[i] = pixels[i][0]
+
+    #image.show()
+
+    addX = (28-newW)//2
+    addY = (28-newH)//2
+
+    # add black border so that the image is 28x28 and the current is centered
+    newPixels = [0 for i in range(28*28)]
+
+    for i in range(0, len(pixels)):
+        x = i % newW
+        y = i // newW
+        newPixels[(y+addY)*28 + x+addX] = pixels[i]
+
+    # save the image to number.data
+    file = open("number.data", "w")
+
+    for i in newPixels:
+        file.write(str(i/255) + " ")
+    file.write("\n")
+    file.close()
---
+++ numberreader.py
@@ -0,0 +1,50 @@
+import pygame
+from pygame.locals import *
+import boundingbox
+import neuralnetwork
+
+pygame.init()
+screen = pygame.display.set_mode((560, 560))
+pygame.display.set_caption("Number Reader")
+
+# set background to white
+background = pygame.Surface(screen.get_size())
+background = background.convert()
+
+# blit background to screen
+screen.blit(background, (0, 0))
+pygame.display.flip()
+draw = False
+
+def save():
+    # save the image as number.jpg
+    pygame.image.save(screen, "number.jpg")
+    boundingbox.boundingbox()
+
+while True:
+    # let the user draw on the screen
+    # resolution is 28x28
+    # position of the mouse
+    m = pygame.mouse.get_pos()
+    if (draw):
+        pygame.draw.circle(screen, (255, 255, 255), m, 28)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+        elif event.type == MOUSEBUTTONDOWN:
+            draw = True
+        elif event.type == MOUSEBUTTONUP:
+            draw = False
+        elif event.type == KEYDOWN:
+            if event.key == K_RETURN:
+                # save the image to number.data
+                save()
+                # clear the screen
+                screen.blit(background, (0, 0))
+                pygame.display.flip()
+                pygame.quit()
+                exit()
+
+    pygame.display.flip()
+
