aba77789ee2cd91b3fd8d5ea698228d7ae9a35d8
pizzamoeger <osshannahsophie@gmail.com>
2023-08-27 01:26:36 +0200

    added CPU data

--- compare/plot.py
+++ compare/plot.py
@@ -1,22 +1,29 @@
 import matplotlib.pyplot as plt
 from scipy import stats
 
-GPU = open("GPU.txt", "r")
+def plot_graph(filename, label, color1, color2):
 
-x_points = []
-y_points = []
-for line in GPU:
-    line = line.split(": ")
-    x_points.append(float(line[0]))
-    y_points.append(float(line[1]))
+    file = open(filename, "r")
 
-slope, intercept, r, p, std_err = stats.linregress(x_points, y_points)
-def myfunc(x):
-    return slope * x + intercept
+    x_points = []
+    y_points = []
+    for line in file:
+        line = line.split(": ")
+        x_points.append(float(line[0]))
+        y_points.append(float(line[1]))
 
-mymodel = list(map(myfunc, x_points))
-plt.plot(x_points, y_points, ":", color='#FF6e6e')
-plt.plot(x_points, mymodel, color='#FF0000', label='Parallel Reduction')
+    slope, intercept, r, p, std_err = stats.linregress(x_points, y_points)
+    def myfunc(x):
+        return slope * x + intercept
+
+    mymodel = list(map(myfunc, x_points))
+    #plt.plot(x_points, y_points, ":", color=color2)
+    plt.scatter(x_points, y_points, 1, color=color2)
+    plt.plot(x_points, mymodel, color=color1, label=label)
+
+plot_graph("GPU.txt", "Parallel Reduction", "#FF0000", "#FF6e6e")
+plot_graph("CPU.txt", "CPU adding", "#2d8108", "#7ebc63")
+
 
 plt.title("time for eval of test_data as a function of size of hidden layer diagram")
 plt.xlabel('neurons in the hidden layer')
