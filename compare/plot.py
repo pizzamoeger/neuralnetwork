1f5a44d93ac19c04584a9e14959d75a9f9e82b9e
pizzamoeger <osshannahsophie@gmail.com>
2023-08-27 09:20:09 +0200

    AtomicAdd data added

--- compare/plot.py
+++ compare/plot.py
@@ -9,7 +9,7 @@ def plot_graph(filename, label, color1, color2):
     y_points = []
     counter = 0
     for line in file:
-        if (counter > 22):
+        if (counter > 176):
             break
         line = line.split(": ")
         x_points.append(float(line[0]))
@@ -21,13 +21,15 @@ def plot_graph(filename, label, color1, color2):
         return slope * x + intercept
 
     mymodel = list(map(myfunc, x_points))
-    plt.plot(x_points, y_points, ":", color=color2)
+    #plt.plot(x_points, y_points, ":", color=color2)
     #plt.scatter(x_points, y_points, 10, color=color2)
-    plt.plot(x_points, mymodel, color=color1, label=label)
+    plt.plot(x_points, y_points, color=color1, label=label)
+    #plt.plot(x_points, mymodel, color=color1, label=label)
 
 plot_graph("GPU.txt", "Parallel Reduction", "#FF0000", "#FF6e6e")
 #plot_graph("CPU.txt", "CPU adding", "#2d8108", "#7ebc63")
 plot_graph("GPU_double.txt", "Parallel Reduction using doubles", "#0b5394", "#6fa8dc")
+#plot_graph("GPU_AtmoicAdd.txt", "AtomicAdd", "#f1c232", "#ffe599")
 
 
 plt.title("time for eval of test_data as a function of size of hidden layer diagram")
